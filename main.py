3a54029cac793749a66368bd3b7ae01e868a3797
Russell Islam <31808766+Russellislam08@users.noreply.github.com>
2020-12-03 10:37:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#watcard_calculator.py
-#This is a simple program which will determine, on average, how much of your meal plan you should spend daily in order to maximise it.
+# watcard_calculator.py
+# This is a simple program which will determine, on average, how much of your meal plan you should spend daily in order to maximise it.
 
 from datetime import datetime
 
-#This function requests for the beginning date that the user would want to input.
+# This function requests for the beginning date that the user would want to input.
 def start_date():
     n = str(input("Residence Start Date (YYYY/MM/DD): "))
     try:
@@ -12,7 +12,7 @@ def start_date():
         print("Incorrect format")
     return dt_start
 
-#This function requests for the end date that the user would want to input. 
+# This function requests for the end date that the user would want to input. 
 def end_date():
     m = str(input("Residence End Date (YYYY/MM/DD): "))
     try:
@@ -21,24 +21,24 @@ def end_date():
         print("Incorrect format")
     return dt_end
 
-#This function determines the number of days within the two dates.
+# This function determines the number of days within the two dates.
 def deltaD(start_date,end_date):
     deltaD = end_date - start_date
     return deltaD.days
 
-#This function determines how many times in total the user will do laundry during the term.
+# This function determines how many times in total the user will do laundry during the term.
 def laundry(days):
     laundry = int(input("Number of times laundry is done in a week: "))
     weeks = days / 7
     return laundry*weeks
 
-#This function calculates the average amount of money spent everyday using everthing calculated before.
+# This function calculates the average amount of money spent everyday using everthing calculated before.
 def dailySpend(laundry, days):
     balance = float(input("Please enter your WatCard Balance: "))
     spend = (balance - (2*laundry))/days
     return spend
 
-#The main function executes everything.
+# The main function executes everything.
 def main():
     start = start_date()
     end = end_date()
